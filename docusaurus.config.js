49c6ecc7d7d78aa5b60a3c7fcb50bf55bcab781f
Jurica Adamek <jura1994jk@hotmail.com>
2023-08-22 12:00:23 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/ESIcon.png',
 
   // Set the production url of your site here
-  url: 'https://superjura.github.io',
+  url: 'https:/docs.escapesimulator.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ESDocs/',
+  baseUrl: '/',
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   
